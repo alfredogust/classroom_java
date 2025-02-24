509b9e7cabc0c67aef6ab3c05e7776ea719685c4
alfredogust <alfredog.fu@gmail.com>
2025-02-24 12:20:14 -0300

    feat: implement sumTwoNumbers function to calculate sum of two user inputs
    This commit adds the 'sumTwoNumbers' function that prompts the user for two integer inputs, calculates their sum, and displays the result.
    - The function uses the Scanner class to read input from the user.
    - The sum is calculated and printed to the console.
    - It returns the computed sum as an integer.
    
    The new functionality enhances the program by allowing dynamic input for basic mathematical operations.

---
+++ src/basic_operations/BasicOperations.java
@@ -0,0 +1,53 @@
+package basic_operations;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public final class BasicOperations {
+
+    public static void helloWorld() {
+        System.out.println("Hello, world!");
+    }
+
+    public static int informedNumber(Scanner scanner) {
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        System.out.printf("The informed number was: %d%n", number);
+        return number;
+    }
+
+    public static void formatAndDisplayData() {
+        String product01 = "Computer";
+        String product02 = "Office desk";
+
+        int age = 30;
+        int code = 5290;
+        char gender = 'F';
+
+        double price01 = 2100.0;
+        double price02 = 650.50;
+        double measure = 53.234567;
+
+        System.out.println("Products:");
+        System.out.printf("%s, which price is $ %.2f%n", product01, price01);
+        System.out.printf("%s, which price is $ %.2f%n", product02, price02);
+        System.out.println();
+        System.out.printf("Record: %d years old, code %d and gender: %c%n", age, code, gender);
+        System.out.println();
+        System.out.printf("Measure with eight decimal places: %.8f%n", measure);
+        System.out.printf("Rounded (three decimal places): %.3f%n", measure);
+        Locale.setDefault(Locale.US);
+        System.out.printf("US decimal point: %.3f%n", measure);
+    }
+
+    public static int sumTwoNumbers(Scanner scanner) {
+        System.out.println("Digite o primeiro numero: ");
+        int firstNumber = scanner.nextInt();
+        System.out.println("Digite o segundo numero: ");
+        int secondNumber = scanner.nextInt();
+
+        int sum = firstNumber + secondNumber;
+        System.out.println("O valor da soma é: " + sum);
+        return sum;
+    }
+}
